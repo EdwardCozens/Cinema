15d74ccb14a20f598d68c523a15f02e8364f8427
Admin <Admin@DESKTOP-M7T70HP>
2024-07-12 18:45:02 +0100

    Add project files.

---
+++ Cinema.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Cinema.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TODO: custom code here
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
